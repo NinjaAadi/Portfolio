187fd6040f7481d299b4087ee55f7fb47d1c3e3a
Aaditya <aaditya7739008423@gmail.com>
2021-01-06 15:12:22 +0530

    Fixed the Home component

--- src/Components/Home/FirstBack/Firstback.js
+++ src/Components/Home/FirstBack/Firstback.js
@@ -6,13 +6,15 @@ import "aos/dist/aos.css";
 
 const Firstback = (props) => {
   useEffect(() => {
-    Aos.init({ duration: 1000 });
+    Aos.init({ duration: 1000, disable: "mobile" });
   }, []);
   return (
     <div className={classes["body"]}>
       <div data-aos="fade-right" className={classes["div1"]}>
         <div className={classes["hello"]}>
-          <h1>Hello, I am </h1>
+          <h1>
+            Hello <i class="fas fa-fist-raised"></i>, I am{" "}
+          </h1>
         </div>
         <div className={classes["name"]}>
           <h1>Aaditya Pal</h1>
@@ -28,6 +30,7 @@ const Firstback = (props) => {
                   "A Competitive Programmer!",
                   "An Extrovert!",
                   "Always ready to learn!",
+                  "Noob Coder!"
                 ],
                 autoStart: true,
                 loop: true,
@@ -35,13 +38,8 @@ const Firstback = (props) => {
             />
           </div>
         </div>
-        <div data-aos="flip-up" className={classes["buttondiv"]}>
-          <button className={classes["aboutus"]}>
-            About Me <i class="fas fa-arrow-down"></i>
-          </button>
-        </div>
       </div>
-      <div className={classes["div2"]} data-aos="slide-down">
+      <div className={classes["div2"]}>
         <div
           className={classes["image"]}
           style={{ backgroundImage: `url(${props.image})` }}
---
+++ src/Components/Home/Secondback/Secondback.js
@@ -0,0 +1,126 @@
+import React, { Fragment, useEffect } from "react";
+import classes from "./Secondback.module.css";
+import profile from "../../../assets/profile.jpeg";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const Secondback = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000, disable: "mobile" });
+  }, []);
+  return (
+    <Fragment>
+      <div className={classes["body"]}>
+        <div className={classes["about"]}>
+          <h1 className={classes["head"]} data-aos="fade-down">
+            About Me
+          </h1>
+        </div>
+        <div className={classes["flex"]}>
+          <div className={classes["div1"]}>
+            <div className={classes["head1"]}>
+              <h1 data-aos="fade-right">
+                Info{" "}
+                <i class="fas fa-info-circle" style={{ color: "#0071c5" }}></i>
+              </h1>
+            </div>
+            <div className={classes["aboutme"]} data-aos="fade-up">
+              <p>
+                Hii , My name is{" "}
+                <b style={{ color: "#a3d2ca", fontSize: "2rem" }}>
+                  Aaditya Pal
+                </b>
+                . I am currently 20 years old. I am a full stack web developer.I
+                also practice Competitive Programming.I am currently a student.
+                My hobbies include novels, driving and music.
+              </p>
+            </div>
+            <div className={classes["label1"]} data-aos="fade-down">
+              <a
+                href="https://www.instagram.com/____aaditya001____/"
+                target="_blank"
+                rel="noreferrer"
+                className={classes["icon"]}
+              >
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/aaditya-pal-7903/"
+                target="_blank"
+                rel="noreferrer"
+                className={classes["icon"]}
+              >
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+              <a
+                href="https://github.com/NinjaAadi"
+                target="_blank"
+                rel="noreferrer"
+                className={classes["icon"]}
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <a
+                href="https://www.codechef.com/users/ninjaaadi7903"
+                target="_blank"
+                rel="noreferrer"
+                className={classes["icon"]}
+              >
+                <span
+                  class="iconify"
+                  data-icon="simple-icons:codechef"
+                  data-inline="false"
+                ></span>
+              </a>
+            </div>
+          </div>
+          <div className={classes["div2"]}>
+            <div className={classes["head1"]}>
+              <h1 data-aos="fade-down">
+                Contact Me{"  "}
+                <i class="fas fa-id-card-alt" style={{ color: "#0071c5" }}></i>
+              </h1>
+            </div>
+            <div className={classes["label"]} data-aos="fade-down">
+              <h2>
+                Email{" "}
+                <i
+                  class="fas fa-envelope-square"
+                  style={{ color: "#008891" }}
+                ></i>
+              </h2>
+              <a
+                className={classes["anc"]}
+                href="mailto:aadityapal.info@gmail.com"
+              >
+                aadityapal.info@gmail.com
+              </a>
+            </div>
+
+            <div className={classes["label"]} data-aos="fade-down">
+              <h2>
+                Phone <i class="fas fa-phone" style={{ color: "#008891" }}></i>
+              </h2>
+              <a className={classes["anc"]} href="tel:+917903966014">
+                +917903966014
+              </a>
+            </div>
+            <div className={classes["label"]} data-aos="fade-down">
+              <h2>
+                Address{" "}
+                <i class="far fa-address-card" style={{ color: "#008891" }}></i>
+              </h2>
+              <p className={classes["anc"]}>
+                Lowadih Chowk, Litch Bagan Namkun Ranchi, Jharkhand
+                <br />
+                Pin : 834010
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Secondback;
