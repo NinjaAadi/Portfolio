1d67f23bdf650ae16c95032cbf3a9afd86362d97
Aaditya <aaditya7739008423@gmail.com>
2021-01-09 23:02:52 +0530

    Fix of UI

---
+++ src/Components/Projects/Carousel/Carousel.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import classes from "./carousel.module.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+export default function Carousel() {
+  useEffect(() => {
+    Aos.init({ duration: 1000, disable: "mobile" });
+  }, []);
+  return (
+    <div className={classes["back"]}>
+      <div data-aos="fade-down" className={classes["card"]}>
+        <h1 className={classes["icon"]}>
+          <i class="fas fa-fighter-jet"></i>{" "}
+        </h1>
+        <h3 className={classes["text"]}>Fast</h3>
+      </div>
+      <div data-aos="fade-down" className={classes["card"]}>
+        <h1 className={classes["icon"]}>
+          <i class="fas fa-mobile-alt"></i>{" "}
+        </h1>
+        <h3 className={classes["text"]}>Responsive</h3>
+      </div>
+      <div data-aos="fade-down" className={classes["card"]}>
+        <h1 className={classes["icon"]}>
+          <i class="fas fa-user-friends"></i>{" "}
+        </h1>
+        <h3 className={classes["text"]}>User-friendly</h3>
+      </div>
+      <div data-aos="fade-down" className={classes["card"]}>
+        <h1 className={classes["icon"]}>
+          <i class="far fa-folder"></i>{" "}
+        </h1>
+        <h3 className={classes["text"]}>Folder Structure</h3>
+      </div>
+    </div>
+  );
+}
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,9 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import classes from "./projects.module.css";
 import back from "../../assets/back.svg";
 import bulb from "../../assets/bulb.svg";
+import Carousel from "./Carousel/Carousel";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Project = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000, enable: "mobile" });
+  }, []);
   return (
     <Fragment>
       <div
@@ -11,13 +17,13 @@ const Project = () => {
         style={{ backgroundImage: `url(${back})` }}
       >
         <div className={classes["head"]}>
-          <h1>
-            My works...{" "}
-            <i class="fas fa-code" style={{ color: "#ff7b54" }}></i>
+          <h1 data-aos="fade-in">
+            My works <i class="fas fa-code" style={{ color: "#ff7b54" }}></i>
           </h1>
         </div>
         <img className={classes["bulb"]} src={bulb} alt="bulb" />
       </div>
+      <Carousel />
     </Fragment>
   );
 };
