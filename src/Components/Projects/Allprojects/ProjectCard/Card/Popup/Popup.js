78f8087930d2c29898e507af27e48e718ab3e7d8
Aaditya <aaditya7739008423@gmail.com>
2021-01-13 17:16:23 +0530

    UI FIX

--- src/Components/Projects/Allprojects/ProjectCard/Card/Popup/Popup.js
+++ src/Components/Projects/Allprojects/ProjectCard/Card/Popup/Popup.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import classes from "./Popup.module.css";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import Carousel from "react-elastic-carousel";
 
 const Popup = (props) => {
   const project = props.project.Project;
@@ -11,7 +12,22 @@ const Popup = (props) => {
         <h1 onClick={props.click} className={classes["cross"]}>
           <i class="far fa-times-circle"></i>{" "}
         </h1>
-        <h1>{project.Name}</h1>
+        <div className={classes["name"]}>
+          <h1>{project.Name}</h1>
+        </div>
+        <div className={classes["heading"]}>
+          <h2>{project.Description}</h2>
+        </div>
+        <div className={classes["carousel"]}>
+          <Carousel itemsToShow={2}>
+            <div className={classes["item"]}>1</div>
+            <div className={classes["item"]}> 2</div>
+            <div className={classes["item"]}>3</div>
+            <div className={classes["item"]}>4</div>
+            <div className={classes["item"]}>5</div>
+            <div className={classes["item"]}>6</div>
+          </Carousel>
+        </div>
       </div>
     </div>
   );
